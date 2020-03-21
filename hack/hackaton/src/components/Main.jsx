b2d55e17effb5d5182472e6d5aa767295fd6b221
katrinnnKat <52584077+katrinnnKat@users.noreply.github.com>
2020-03-21 16:36:21 +0500

    base setting

---
+++ hack/hackaton/src/components/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Toolbar from './Toolbar';
+
+
+export default class Main extends React.Component {
+    render() {
+        return (
+            <div className="content">
+                <Toolbar />
+            </div>
+        )
+    }
+}
